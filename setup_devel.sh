d87de7062f6c9212cef85869664e6034fbd8a475
Shimon Samefu <shimon-samefu@outlook.com>
2021-08-17 08:39:28 -0400

    init

---
+++ setup_devel.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sudo dnf update
+sudo dnf -y groupinstall "Development Tools"
+sudo dnf -y install alsa-lib-devel
+sudo dnf -y install atk-devel
+sudo dnf -y install bzip2-devel
+sudo dnf -y install cairo-devel
+sudo dnf -y install gdbm-devel
+sudo dnf -y install gdk-pixbuf2-devel
+sudo dnf -y install ghc-pango-devel
+sudo dnf -y install glib2-devel
+sudo dnf -y install gtk3-devel
+sudo dnf -y install libffi-devel        
+sudo dnf -y install libsoup-devel
+sudo dnf -y install libuuid-devel
+sudo dnf -y install libXxf86vm-devel
+sudo dnf -y install ncurses-devel
+sudo dnf -y install openssl-devel
+sudo dnf -y install pcre-devel
+sudo dnf -y install pulseaudio-libs-devel
+sudo dnf -y install readline-devel
+sudo dnf -y install sqlite-devel
+sudo dnf -y install tk-devel
+sudo dnf -y install webkit2gtk3-devel
+sudo dnf -y install xz-devel
+sudo dnf -y install zlib-devel
